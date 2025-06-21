b39eb6c5b487bf55f6c3ce7d4d0c543b1f4dd224
Matthew <vo23673@bristol.ac.uk>
2025-06-21 02:16:37 +0100

    Initial Project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello, Vite + React!</h1>
+      <p>This is your React app running inside Vite.</p>
+    </div>
+  );
+}
