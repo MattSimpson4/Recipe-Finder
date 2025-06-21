c4eb1af08f84d125de6876553ddd4471d548b527
Matthew <vo23673@bristol.ac.uk>
2025-06-21 19:08:17 +0100

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 export default function App() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Hello, Vite + React!</h1>
-      <p>This is your React app running inside Vite.</p>
-    </div>
+    <>
+      <input></input>
+    </>
   );
 }
