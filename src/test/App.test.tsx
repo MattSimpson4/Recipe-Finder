93ac1f07a0fda977bf614a68d3378551595b7ea8
Matthew <vo23673@bristol.ac.uk>
2025-07-17 21:44:45 +0100

    Updated Tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders hello message', () => {
-  render(<App />);
-  expect(screen.getByText(/Hello/i)).toBeInTheDocument();
+test('renders the welcome message', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/welcome to recipe finder/i)).toBeInTheDocument();
+});
+test('renders the sign up button', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByRole('button', { name: /sign up/i })).toBeInTheDocument();
+});
+test('renders the log in button', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
 });
